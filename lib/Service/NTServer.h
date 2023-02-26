3812caa5d7dd44321705db841a7b815a94d6134c
greyes <gabryelrdiaz@gmail.com>
2023-02-26 23:35:37 +0100

    Added Frame definition

--- lib/Service/NTServer.h
+++ lib/Service/NTServer.h
@@ -120,6 +120,45 @@ private:
         }
     };
 
+    /** Channel Field Length in Bytes */
+    static const uint8_t CHANNEL_LEN = 1U;
+
+    /** DLC Field Length in Bytes */
+    static const uint8_t DLC_LEN = 1U;
+
+    /** Data Field Length in Bytes */
+    static const uint8_t MAX_DATA_LEN = 8U;
+
+    /** Checksum Field Length in Bytes */
+    static const uint8_t CHECKSUM_LEN = 1U;
+
+    /** Total Frame Length in Bytes */
+    static const uint8_t MAX_FRAME_LEN = CHANNEL_LEN + DLC_LEN + MAX_DATA_LEN + CHECKSUM_LEN;
+
+    /** Data container of the Frame Fields */
+    typedef union _Frame
+    {
+        /** Frame Fields */
+        struct _Fields
+        {
+            /** Channel ID */
+            uint8_t m_channel;
+
+            /** Data Length */
+            uint8_t m_dlc;
+
+            /** Data of the Frame */
+            uint8_t m_data[MAX_DATA_LEN];
+
+            /** Frame Checksum */
+            uint8_t m_checksum;
+        } __attribute__((packed)) fields;
+
+        /** Raw Frame Data */
+        uint8_t raw[MAX_FRAME_LEN];
+
+    } __attribute__((packed)) Frame;
+
     /**
      *  Array of Data Channels.
      */
